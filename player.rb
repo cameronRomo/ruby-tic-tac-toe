2886c8b37d07c1daeb33c897a6ad679e28797eb3
Cameron Romo <cromo1225@gmail.com>
2022-12-08 23:09:40 -0700

    Refactor lambda call and reformat prompt method

--- player.rb
+++ player.rb
@@ -14,19 +14,21 @@ class Player
   end
 
   def get_move
-    [get_coordinate(:row), get_coordinate(:column)]
+    TRANSLATIONS.map do |row_or_column, translator|
+      get_coordinate row_or_column, translator
+    end
   end
 
-  def prompt_choice string
-    puts "Pick a #{string}"
-    print "> "
+  private
+  def prompt_choice row_or_column
+    puts "Pick a #{row_or_column}\n>"
   end
 
-  def get_coordinate row_or_column
+  def get_coordinate row_or_column, translator
     loop do
-      prompt_choice row_or_column
+      print prompt_choice row_or_column
       input = gets
-      choice = TRANSLATIONS[row_or_column].call(input)
+      choice = translator.call(input)
       return choice if valid? choice
       puts "Invalid #{row_or_column}."
     end
