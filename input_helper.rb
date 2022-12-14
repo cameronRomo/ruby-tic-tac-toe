11d4cdfe2cb3c816f2e232b38bfab4e384e97e20
Cameron Romo <cromo1225@gmail.com>
2022-12-14 10:25:11 -0700

    Add ability to easily quit at anytime.

--- input_helper.rb
+++ input_helper.rb
@@ -1,5 +1,17 @@
 module InputHelper
+  SAFE_WORDS = ['QUIT', 'EXIT', 'GET ME OUT OF HERE!']
+
   def prompt string
     print string + "\n> "
+  end
+
+  def gets
+    input = Kernel.gets
+    if SAFE_WORDS.include?(input.chomp.strip.upcase)
+      puts "User quit."
+      puts "Goodbye."
+      exit
+    end
+    input
   end
 end
